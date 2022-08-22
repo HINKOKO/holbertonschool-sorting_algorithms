f6c72621ccb71a881462c1548cc267887fcf7b15
root <piotr62@live.fr>
2022-08-22 05:10:05 -0700

    cleaning files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,7 @@ void swap(int *x, int *y)
 }
 
 /**
- * bubble_sort : sorts an array using Bubblesort algo
+ * bubble_sort - sorts an array using Bubblesort algo
  * @array: the array to sort
  * @size: size of the array
  */
@@ -20,13 +20,16 @@ void swap(int *x, int *y)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
+			{
 				swap(&array[j], &array[j + 1]);
-			print_array(array, size);
+				print_array(array, size);
+			}
 		}
 	}
 }
