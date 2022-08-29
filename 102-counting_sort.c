be884ae8ef7b00676614f7347760fc8c60038dc3
NehemieMbg <nehemie.mbg@gmail.com>
2022-08-29 13:11:55 +0000

    hot fix

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -13,25 +13,19 @@ void counting_sort(int *array, size_t size)
 	int max = -1;
 	size_t *output;
 	int *count = NULL;
-
-	/* Find the largest element in the array */
 	for (index = 0; index < (int)size; index++)
 	{
 		if (array[index] > max)
 			max = array[index];
 	}
 	max = max + 1;
-	/* Giving memory to count and Initializing count array with all zeros */
 	count = calloc(sizeof(int), max);
-	/* Protecting count if it doesn't exist return */
 	if (!count)
 		return;
-	/* Store count of each elements */
 	for (index = 0; index < (int)size; index++)
 	{
 		count[array[index]] += 1;
 	}
-	/* Store the cumulative count of each array */
 	for (index = 0; index < max; index++)
 	{
 		count[index] += count[index - 1];
@@ -40,7 +34,6 @@ void counting_sort(int *array, size_t size)
 	output = calloc(size, sizeof(int));
 	if (!output)
 		return;
-	/* Place the elements in output array */
 	for (index = size - 1; index >= 0; index--)
 	{
 		output[count[array[index]] - 1] = array[index];
